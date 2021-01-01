af4e56a00625a9ba098554c89d913d32d03cef99
anonymous-00110000 <73768640+anonymous-00110000@users.noreply.github.com>
2021-01-01 10:39:05 +0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,15 +2,14 @@
 @section('content')
 <div class="row">
     @foreach ($data as $key )
-    <div class="col-sm-6">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">{{ $key['Id_number'] }}</h5>
-            <p class="card-text">{{ $key['Lname'] }},&nbsp;{{ $key['Fname'] }}</p>
+        <div class="col-sm-6 pb-2">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">{{ $key['Id_number'] }}</h5>
+              <p class="card-text">{{ $key['Lname'] }},&nbsp;{{ $key['Fname'] }}</p>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
     @endforeach
 
 @endsection
